e0a8ffaafbbaec61bf128327f25f4e8694d23f9f
inlabdzc <1521575734@qq.com>
2024-09-01 09:43:30 +0800

    new probs

---
+++ 72.编辑距离.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=72 lang=cpp
+ *
+ * [72] 编辑距离
+ */
+
+// @lc code=start
+#include<string>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int len1 = word1.size();
+        int len2 = word2.size();
+
+        // dp[i][j] : word1的前i个元素与word2的前j个元素的最小编辑次数
+        vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1, 0));
+        // 初始化
+        for(int i = 1; i <= len2; i++) dp[0][i] = i;
+        for(int i = 1; i <= len1; i++) dp[i][0] = i;
+
+        for(int i = 1; i <= len1; i++)
+        {
+            for(int j = 1; j <= len2; j++)
+            {
+                if(word1[i - 1] == word2[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+                else dp[i][j] = min(min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+            }
+        }
+
+        return dp[len1][len2];
+    }
+};
+// @lc code=end
+
