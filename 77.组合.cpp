9a71342de2225ec69d32e46a4b3894688fb1465b
inlabdzc <1521575734@qq.com>
2024-12-17 13:21:48 +0800

    2024.12.17

--- 77.组合.cpp
+++ 77.组合.cpp
@@ -12,6 +12,11 @@ class Solution {
 public:
     vector<vector<int>> totalRes;
     vector<int> currRes;
+
+    // 算法的二叉树:
+    // -------------------------
+    //    1       2      3     4
+    // 2  3  4   3 4     4
     void dfs(int n, int k, int startIndex)
     {
         if(currRes.size() == k) 
@@ -20,14 +25,14 @@ public:
             return;
         }
         // 当前已选 currRes.size() 个,还差 k - currRes.size()个
-        // 从当前开始还剩的元素个数为: n - i + 1 个
+        // 从当前开始还剩的可选元素个数为: n - i + 1 个
         // 所以剪枝判断: n - i + 1 >= k - currRes.size()
-        // 即:i <= n + 1 - (k - currRes.size())
+        // 即 : i <= n + 1 - (k - currRes.size())
         for(int i = startIndex; i <= n + 1 - (k - currRes.size()); i++)
         {
             currRes.push_back(i);
             dfs(n, k, i + 1);
-            currRes.pop_back(); // 回溯的关键!
+            currRes.pop_back(); 
         }
     }
     vector<vector<int>> combine(int n, int k) {
