4225259e8339949c6772276721b9348793dc33f1
inlabdzc <1521575734@qq.com>
2024-12-25 20:21:26 +0800

    2024.12.25

--- 76.最小覆盖子串.cpp
+++ 76.最小覆盖子串.cpp
@@ -5,11 +5,63 @@
  */
 
 // @lc code=start
+#include <cstdint>
+#include <string>
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
-    string minWindow(string s, string t) {
-
+  string minWindow(string s, string t) {
+    // valid 记录 t 中的字符, 有或者没有
+    vector<bool> valid(128, false);
+    // freq 记录目前 t 中每个字符在 s 的滑动窗口中缺少的数量
+    // 如果为正, 则说明还缺少；如果为负, 则说明有盈余。
+    vector<int> freq(128, 0);
+    for (int i = 0; i < t.length(); ++i) {
+      valid[t[i]] = true;
+      ++freq[t[i]];
     }
+
+    // count 代表已经被包含的字符个数,重复的也会算
+    int count = 0;
+    // 分别代表最优解的left和length
+    int min_left = -1, min_len = INT32_MAX;
+    // 枚举 s 字串的右端点 right, 如果子串涵盖 t，就不断移动左端点 left
+    // 直到不涵盖为止。 在移动过程中更新最短子串的左右端点。
+    for (int left = 0, right = 0; right < s.length(); ++right) {
+      // 当前 s[right] 非 valid, 直接跳过
+      if (!valid[s[right]]) {
+        continue;
+      }
+
+      // 把 right 位置的字符加入频率统计
+      freq[s[right]]--;
+      // 并检查是否补充了缺失的字符
+      if (freq[s[right]] >= 0) {
+        ++count;
+      }
+
+      // 在滑动窗口已包含t中全部字符的情况下
+      while (count == t.length()) {
+        // 更新全局变量 min_left 和 min_len
+        if (right - left + 1 < min_len) {
+          min_left = left;
+          min_len = right - left + 1;
+        }
+
+        // 尝试右移left，在不影响结果的情况下寻找最短串。
+        // 将left的字符移除频率统计,并检查t中对应字符是否重新缺失
+        if (valid[s[left]]) {
+          freq[s[left]]++;
+          if (freq[s[left]] > 0) {
+            --count;
+          }
+        }
+        left++;
+      }
+    }
+    return min_len == INT32_MAX ? "" : s.substr(min_left, min_len);
+  }
 };
 // @lc code=end
-
--- 88.合并两个有序数组.cpp
+++ 88.合并两个有序数组.cpp
@@ -5,47 +5,35 @@
  */
 
 // @lc code=start
-#include<vector>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        if(m == 0)
-        {
-            nums1 = nums2;
-        }
-
-        else
-        {
-            for(int i = m - 1, j = n - 1; i > -1 && j > -1;)
-            {
-                // 依次比较末尾的数谁大，然后往num1向前塞
-                if(nums2[j] >= nums1[i])
-                {
-                    nums1[i + j + 1] = nums2[j];
-                    --j;
-                }
-                else
-                {
-                    nums1[i + j + 1] = nums1[i];
-                    --i;
-                }
-
-                // 如果nums2已经用完了,而nums1还没遍历完，则正常结束
-                // 下面要处理,num1已经用完了,但nums2还没用完:
-                // 将nums2剩下的数挪到num1的前边。
-                if(i == -1 && j >= 0)
-                {
-                    for(j; j >= 0; --j)
-                    {
-                        nums1[j] = nums2[j];
-                    }
-                    j--;
-                }
-            }
-        }
+  void merge(vector<int> &nums1, int m, vector<int> &nums2, int n) {
+    if (m == 0) {
+      nums1 = nums2;
     }
+    for (int i = m - 1, j = n - 1; i >= 0 && j >= 0;) {
+      // 依次比较末尾的数谁大，然后往 num1 从后向前塞
+      if (nums2[j] >= nums1[i]) {
+        nums1[i + j + 1] = nums2[j];
+        --j;
+      } else {
+        nums1[i + j + 1] = nums1[i];
+        --i;
+      }
+
+      // 如果 nums2 已经用完了,而 nums1 还没遍历完, 则 nums1 已经有序,正常结束
+      // 如果 num1 已经用完了,但 nums2 还没用完, 则要将 nums2 剩下的数挪到 num1
+      // 的前边
+      if (i == -1 && j >= 0) {
+        while (j >= 0) {
+          nums1[j] = nums2[j];
+          j--;
+        }
+      }
+    }
+  }
 };
 // @lc code=end
-
